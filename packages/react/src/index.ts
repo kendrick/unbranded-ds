e6641304b28ff033bff22e5e2b359501bed71e18
Kendrick Arnett <kmarnett@gmail.com>
2026-04-11 17:54:52 -0500

    feat(react): barrel exports for all components
    Closes #43, #44

---
+++ packages/react/src/index.ts
@@ -0,0 +1,13 @@
+// Components
+export * from "./components/Button";
+export * from "./components/Input";
+export * from "./components/Label";
+export * from "./components/Card";
+export * from "./components/Dialog";
+export * from "./components/Select";
+export * from "./components/Checkbox";
+export * from "./components/Switch";
+export * from "./components/Tabs";
+
+// Utilities
+export { cn } from "./lib/cn";
