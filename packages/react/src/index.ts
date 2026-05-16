c5653daf117bd72f9ed9599138080dcb23f05958
Kendrick Arnett <kmarnett@gmail.com>
2026-05-16 17:08:57 -0500

    feat(react): wire root exports for spec 004 primitives
    Adds the four new components (SegmentedControl, SkipLink, Slider, Tooltip) to the package barrel so consumers can import them with the existing pattern: import { Tooltip } from '@unbranded-ds/react'.
    
    Implements T008, T014, T020, T026 from spec 004 tasks.md.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -5,9 +5,13 @@ export * from './components/Checkbox';
 export * from './components/Dialog';
 export * from './components/Input';
 export * from './components/Label';
+export * from './components/SegmentedControl';
 export * from './components/Select';
+export * from './components/SkipLink';
+export * from './components/Slider';
 export * from './components/Switch';
 export * from './components/Tabs';
+export * from './components/Tooltip';
 export * from './components/VisuallyHidden';
 
 // Utilities
